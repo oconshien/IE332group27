f2c06de70680a0fde7ec91edd12537a9e7edc372
Brad Hicks <oconshien@gmail.com>
2019-10-24 16:30:10 -0400

    mar.noname has pm3

--- g27/MLclassifier.R
+++ g27/MLclassifier.R
@@ -13,4 +13,23 @@ require(stringr) #more data wrangling
 require(ggridges) #plotting density ridges
 require(tibble) #as_tibble, easy to use
 
-march <- as_tibble(fread("air-quality-data-from-extensive-network-of-sensors/march-2017.csv"))
+march = as_tibble(fread("air-quality-data-from-extensive-network-of-sensors/march-2017.csv"))
+
+Sys.setenv(TZ='Poland') #we're looking at data from Poland, to avoid erors we'll use this command. If this is not given a timezone error will appear.
+march$`UTC time` = as_datetime(march$`UTC time`)
+mar.test = march %>% select(contains("pm"))
+# create a function to replace NA values with the median
+medrep = function(i){
+  i[is.na(i)] = median(i, na.rm=TRUE) 
+  as.numeric(i)
+}
+mar.med = data.frame(apply(mar.test,2,medrep))
+
+mar.noname = data.frame(
+  pm010 = mar.med$X3_pm1,
+  pm025 = mar.med$X3_pm25,
+  pm100 = mar.med$X3_pm10
+)
+
+#eventually omit NA
+nona.df = na.omit(raw.df)
