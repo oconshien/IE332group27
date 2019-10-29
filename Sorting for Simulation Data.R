ef383dadee4f72996a9e0a248691a9b72b80d9e0
roachl <55259642+roachl@users.noreply.github.com>
2019-10-29 10:47:41 -0400

    Add files via upload

---
+++ Sorting for Simulation Data.R
@@ -0,0 +1,246 @@
+#Use a system of if statements that are contingent on a system of defining what the month is
+
+  
+  #convert seconds to month in some way
+  if month >= 1 | month <= 3{
+    if 
+  }
+  if (sensor = 169 | sensor = 219 | sensor = 225){
+    
+  }
+  if (sensor = 204 | sensor = 194 | sensor = 178){
+    
+  } 
+  if (sensor = 185 | sensor = 182 | sensor = 219){
+    
+  }
+  if ()
+ 
+    
+#Loop Idea
+  while fit$x[i] != inputDate{
+    if (sensor = 204 | sensor = 194 | sensor = 178){
+        partMat <- fit$y[i]
+     }
+    if (sensor = 169 | sensor = 219 | sensor = 225){
+        partMat <- fit$y[i]
+      }
+    if (sensor = 185 | sensor = 182 | sensor = 219){
+        partMat <- fit$y[i]
+      }
+    i = i+1
+    }
+#Or While Months  
+  #some kind of formula to convert months to inputted time format
+  jan <- 
+  feb <-
+  march <- 
+  apr <-
+  may <- 
+  june <- 
+  july <- 
+  aug <-
+  sept <-
+  oct <-
+  nov <- 
+  dec <-
+  
+  if time > feb{ #January
+    while fit$x[i] != inputTime{
+      i = i+1
+    }
+      if (sensor = 204 | sensor = 194 | sensor = 178){
+        partMat <- fit$y[i]
+        }
+      if (sensor = 169 | sensor = 219 | sensor = 225){
+        partMat <- fit$y[i]
+        }
+      if (sensor = 185 | sensor = 182 | sensor = 219){
+        partMat <- fit$y[i]
+      }
+  }
+  if time >= feb & time < march{ #February
+      while fit$x[i] != inputTime{
+         i = i+1
+        }
+        if (sensor = 204 | sensor = 194 | sensor = 178){
+         partMat <- fit$y[i]
+        }
+        if (sensor = 169 | sensor = 219 | sensor = 225){
+         partMat <- fit$y[i]
+        }
+        if (sensor = 185 | sensor = 182 | sensor = 219){
+         partMat <- fit$y[i]
+        }
+  }
+  if time >= march & time < apr{ #March
+        while fit$x[i] != inputTime{
+         i = i+1
+        }
+        if (sensor = 204 | sensor = 194 | sensor = 178){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 169 | sensor = 219 | sensor = 225){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 185 | sensor = 182 | sensor = 219){
+          partMat <- fit$y[i]
+        }
+  }
+  if time >= apr & time < may{ #April
+        while fit$x[i] != inputTime{
+          i = i+1
+        }
+        if (sensor = 204 | sensor = 194 | sensor = 178){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 169 | sensor = 219 | sensor = 225){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 185 | sensor = 182 | sensor = 219){
+          partMat <- fit$y[i]
+        }
+  }
+  if time >= may & time < june{ #May
+        while fit$x[i] != inputTime{
+          i = i+1
+        }
+        if (sensor = 204 | sensor = 194 | sensor = 178){
+         partMat <- fit$y[i]
+        }
+        if (sensor = 169 | sensor = 219 | sensor = 225){
+         partMat <- fit$y[i]
+        }
+        if (sensor = 185 | sensor = 182 | sensor = 219){
+         partMat <- fit$y[i]
+        }
+  }
+  if time >= june & time < july { #June
+        while fit$x[i] != inputTime{
+        i = i+1
+        }
+        if (sensor = 204 | sensor = 194 | sensor = 178){
+         partMat <- fit$y[i]
+        }
+        if (sensor = 169 | sensor = 219 | sensor = 225){
+         partMat <- fit$y[i]
+        }
+        if (sensor = 185 | sensor = 182 | sensor = 219){
+         partMat <- fit$y[i]
+        }
+  }
+  if time >= july & time < aug { #July
+        while fit$x[i] != inputTime{
+         i = i+1
+        }
+        if (sensor = 204 | sensor = 194 | sensor = 178){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 169 | sensor = 219 | sensor = 225){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 185 | sensor = 182 | sensor = 219){
+          partMat <- fit$y[i]
+        }
+  }
+  if time >= aug & time < sept{ #August
+        while fit$x[i] != inputTime{
+          i = i+1
+        }
+        if (sensor = 204 | sensor = 194 | sensor = 178){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 169 | sensor = 219 | sensor = 225){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 185 | sensor = 182 | sensor = 219){
+          partMat <- fit$y[i]
+        }
+  }
+  if time >= sept & time < oct{ #September
+        while fit$x[i] != inputTime{
+          i = i+1
+        }
+        if (sensor = 204 | sensor = 194 | sensor = 178){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 169 | sensor = 219 | sensor = 225){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 185 | sensor = 182 | sensor = 219){
+          partMat <- fit$y[i]
+        }
+  }
+  if time >= oct & time < nov{ #October
+        while fit$x[i] != inputTime{
+          i = i+1
+        }
+        if (sensor = 204 | sensor = 194 | sensor = 178){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 169 | sensor = 219 | sensor = 225){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 185 | sensor = 182 | sensor = 219){
+          partMat <- fit$y[i]
+        }
+  }
+  if time >= nov & time < dec{ #November
+        while fit$x[i] != inputTime{
+          i = i+1
+        }
+        if (sensor = 204 | sensor = 194 | sensor = 178){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 169 | sensor = 219 | sensor = 225){
+          partMat <- fit$y[i]
+        }
+        if (sensor = 185 | sensor = 182 | sensor = 219){
+          partMat <- fit$y[i]
+        }
+  }
+  if time >= dec {#December
+        while fit$x[i] != inputTime{
+          i = i+1
+        }
+        if (sensor = 204 | sensor = 194 | sensor = 178){
+          partMat <- fit$y[i]
+            if partMat <= 25 & partMat > 10{
+              
+            }
+            if partMat <= 10 & partMat > 1{
+              
+            }
+            if partMat <= 1{
+              
+            }
+          }
+        }
+        if (sensor = 169 | sensor = 219 | sensor = 225){
+          partMat <- fit$y[i]
+          if partMat <= 25 & partMat > 10{
+            
+          }
+          if partMat <= 10 & partMat > 1{
+            
+          }
+          if partMat <= 1{
+            
+          }
+        }
+        if (sensor = 185 | sensor = 182 | sensor = 219){
+          partMat <- fit$y[i]
+          if partMat <= 25 & partMat > 10{
+            
+          }
+          if partMat <= 10 & partMat > 1{
+            
+          }
+          if partMat <= 1{
+            
+          }
+        }
+  }
+
+  print(partMat)
+
