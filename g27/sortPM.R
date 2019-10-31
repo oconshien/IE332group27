aa844ec17186d76544b53cfaac55047b78ec6291
roachl <55259642+roachl@users.noreply.github.com>
2019-10-31 16:19:32 -0400

    Add files via upload
    sorts the data

---
+++ g27/sortPM.R
@@ -0,0 +1,20 @@
+
+sortPM <- function(month, region){
+inputDate <- month$x
+while (month$x[i] != inputDate){
+  if region = 0{
+    sample(c(169, 219, 225))  
+    partMat <- fit$y[i]
+  }
+  if region = 1{
+    sample(c(204, 194, 178))  
+    partMat <- fit$y[i]
+  }
+  
+  if region = 2{
+    sample(c(185, 182, 219))
+    partMat <- fit$y[i]
+  }
+  i = i+1
+}
+}
