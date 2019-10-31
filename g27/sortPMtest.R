d4bf6d98427d7e81a51257f002243a4a26b73524
TrumanBoggs <55259630+Truman1928@users.noreply.github.com>
2019-10-31 16:54:28 -0400

    Update sortPMtest.R

--- g27/sortPMtest.R
+++ g27/sortPMtest.R
@@ -1,43 +1,23 @@
-sortPM <- function(input_time, region){}
+sortPM <- function(input_time, region){
   month <- month(input_time, label = TRUE, abbr = FALSE)
   print(month)
     if (region == 0){
       sensor_num <- sample(c(169, 219, 225))  
-      #smooth_reg_month_maker(sensor_num, 1, month)
-      #smooth_reg_month_maker(sensor_num, 25, month)
-      #smooth_reg_month_maker(sensor_num, 10, month)
+      smooth_reg_month_maker(sensor_num, 1, month)
+      smooth_reg_month_maker(sensor_num, 25, month)
+      smooth_reg_month_maker(sensor_num, 10, month)
     }
     if (region == 1){
       sensor_num <- sample(c(204, 194, 178))  
-      #smooth_reg_month_maker(sensor_num, 1, month)
-      #smooth_reg_month_maker(sensor_num, 25, month)
-      #smooth_reg_month_maker(sensor_num, 10, month)
+      smooth_reg_month_maker(sensor_num, 1, month)
+      smooth_reg_month_maker(sensor_num, 25, month)
+      smooth_reg_month_maker(sensor_num, 10, month)
     }
     
     if (region == 2){
       sensor_num <- sample(c(185, 182, 219))
-      #smooth_reg_month_maker(sensor_num, 1, month)
-      #smooth_reg_month_maker(sensor_num, 25, month)
-      #smooth_reg_month_maker(sensor_num, 10, month)
+      smooth_reg_month_maker(sensor_num, 1, month)
+      smooth_reg_month_maker(sensor_num, 25, month)
+      smooth_reg_month_maker(sensor_num, 10, month)
     }
-=======
-
-sortPM <- function(month, region){
-inputDate <- month$x
-while (month$x[i] != inputDate){
-  if region = 0{
-    sample(c(169, 219, 225))  
-    partMat <- fit$y[i]
-  }
-  if region = 1{
-    sample(c(204, 194, 178))  
-    partMat <- fit$y[i]
-  }
-  
-  if region = 2{
-    sample(c(185, 182, 219))
-    partMat <- fit$y[i]
-  }
-  i = i+1
-}
 }
