10ec81cac595944102e556f6f969beda1b6cd28a
TrumanBoggs <55259630+Truman1928@users.noreply.github.com>
2019-11-03 15:24:24 -0500

    Update sortPMtest.R

--- g27/sortPMtest.R
+++ g27/sortPMtest.R
@@ -5,22 +5,22 @@ sortPM <- function(input_time, region){
   print(month)
     if (region == 0){
       sensor_num <- sample(c(169, 219, 225))  
-      pm_df[1] <- smooth_reg_month_maker(sensor_num, 1, month, time_since)
-      pm_df[2] <- smooth_reg_month_maker(sensor_num, 25, month, time_since)
-      pm_df[3] <- smooth_reg_month_maker(sensor_num, 10, month, time_since)
+      pm_df[1,] <- smooth_reg_month_maker(sensor_num, 1, month, time_since)
+      pm_df[2,] <- smooth_reg_month_maker(sensor_num, 25, month, time_since)
+      pm_df[3,] <- smooth_reg_month_maker(sensor_num, 10, month, time_since)
     }
     if (region == 1){
       sensor_num <- sample(c(204, 194, 178))  
-      pm_df[1] <- smooth_reg_month_maker(sensor_num, 1, month, time_since)
-      pm_df[2] <- smooth_reg_month_maker(sensor_num, 25, month, time_since)
-      pm_df[3] <- smooth_reg_month_maker(sensor_num, 10, month, time_since)
+      pm_df[1,] <- smooth_reg_month_maker(sensor_num, 1, month, time_since)
+      pm_df[2,] <- smooth_reg_month_maker(sensor_num, 25, month, time_since)
+      pm_df[3,] <- smooth_reg_month_maker(sensor_num, 10, month, time_since)
     }
     
     if (region == 2){
       sensor_num <- sample(c(185, 182, 219))
-      pm_df[1] <- smooth_reg_month_maker(sensor_num, 1, month, time_since)
-      pm_df[2] <- smooth_reg_month_maker(sensor_num, 25, month, time_since)
-      pm_df[3] <- smooth_reg_month_maker(sensor_num, 10, month, time_since)
+      pm_df[1,] <- smooth_reg_month_maker(sensor_num, 1, month, time_since)
+      pm_df[2,] <- smooth_reg_month_maker(sensor_num, 25, month, time_since)
+      pm_df[3,] <- smooth_reg_month_maker(sensor_num, 10, month, time_since)
     }
   return(pm_df)
 }
