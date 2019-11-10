c493168425de0aa402d48ea10bc3bb5c51414309
jfinucane822 <jfinucane@att.net>
2019-11-10 15:45:02 -0500

    Update move_mobile.R

--- g27/move_mobile.R
+++ g27/move_mobile.R
@@ -1,7 +1,7 @@
 nearest_sensor <- function(destination, sensors, quality){
   mobile_sensors <- sensors[which(sensors[,3]==1),]
   num_sensors <- dim(mobile_sensors)[1]
-  for(j in 1:length(destination[1,])){
+  for(j in 1:length(destination[,1])){
     dist_vec <- rep(0,num_sensors)
     for(i in 1:num_sensors){
       dist_vec[i] <- sqrt((mobile_sensors[i,1]-destination[j,1])^2+(mobile_sensors[i,2]-destination[j,2])^2)
@@ -10,7 +10,7 @@ nearest_sensor <- function(destination, sensors, quality){
     #good vs bad air quality choice
     nearest_sensor <- which(dist_vec == min(dist_vec[dist_vec!=min(dist_vec)]))
     near_sensor <- which(mobile_sensors[nearest_sensor,1] == sensors[,1] & mobile_sensors[nearest_sensor,2] == sensors[,2])
-    moved_sensor <- mobile_sensor(dist_vec[nearest_sensor],destination[j,], sensors[near_sensor,])
+    moved_sensor <- mobile_sensor(dist_vec[nearest_sensor],as.integer(destination[j,]), sensors[near_sensor,])
     sensors[near_sensor,1] <- moved_sensor[1] + sensors[near_sensor,1]
     sensors[near_sensor,2] <- moved_sensor[2] + sensors[near_sensor,2]
   }
