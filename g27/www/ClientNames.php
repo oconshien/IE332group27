65fa34a291586150f24336aba7900c1fda38ba1a
Brad Hicks <oconshien@gmail.com>
2019-11-12 10:12:26 -0500

    www updated with 3 bio pages
    html file extension hidden in url

--- g27/www/ClientNames.php
+++ g27/www/ClientNames.php
@@ -1,47 +1,246 @@
-<?php
-$servername = "mydb.itap.purdue.edu";
-$username = "g1109699";
-$password = "MySQL27";
-$dbname = "g1109699";
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+    <title>Project Website</title>
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-$sql = "SELECT C_ID, company, cFirst FROM Client";
+    <!-- Custom CSS: You can use this stylesheet to override any Bootstrap styles and/or apply your own styles -->
+    <link href="css/custom.css" rel="stylesheet">
 
-$result = $conn->query($sql);
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+	<style>
+	th {
+		padding: 15px;
+		background-color: #4CAF50;
+		color: white;
+		border-color: black;
+		}
+		
+	td {
+		color: black;
+		padding: 5px;
+	}
+	tr:nth-child(even) {background-color: #d2d2d2;}
+	
+	</style>
 
-echo "<table border='1'>
-<tr>
-<th>Client ID</th>
-<th>Company</th>
-<th>First Name</th>
-</tr>";
 
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['C_ID'] . "</td>";
-echo "<td>" . $row['company'] . "</td>";
-echo "<td>" . $row['cFirst'] . "</td>";
-echo "</tr>";
-}
-echo "</table>";
+</head>
 
-// if ($result->num_rows > 0) {
-    // output data of each row
-    // while($row = $result->fetch_assoc()) {
-        // echo "<br> Client Number: " . $row["C_ID"]. " - ". $row["company"]. "<br>";
-    // }
-// } else {
-    // echo "0 results";
-// }
+<body>
 
-$conn->close();
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Logo and responsive toggle -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="https://web.ics.purdue.edu/~g1109699/index">
+                	<span class="glyphicon glyphicon-grain"></span> 
+                	Group 27
+                </a>
+            </div>
+            <!-- Navbar links -->
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="https://web.ics.purdue.edu/~g1109699/index">Home</a>
+                    </li>
+                    <li>
+                        <a href="https://web.ics.purdue.edu/~g1109699/Solutions">Solutions</a>
+                    </li>
+                    <li>
+                        <a href="https://web.ics.purdue.edu/~g1109699/Technology">Technology</a>
+                    </li>
+					<li class="active">
+                        <a href="https://web.ics.purdue.edu/~g1109699/HistoricalData">Historical Data</a>
+                    </li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Meet the Team <span class="caret"></span></a>
+						<ul class="dropdown-menu" aria-labelledby="about-us">
+							<li><a href="#">Will Hicks</a></li>
+							<li><a href="#">Jeff Finucane</a></li>
+							<li><a href="#">Laura Roach</a></li>
+							<li><a href="#">Truman Boggs</a></li>
+							<li><a href="#">Jorge Valdez</a></li>
+							<li><a href="https://web.ics.purdue.edu/~g1109699/gharoldPage">Giovanni Harold</a></li>
+						</ul>
+					</li>
+                </ul>
 
-?>
+				
+
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+	<div class="jumbotron feature">
+		<div class="container">
+		</div>
+	</div>
+
+    <!-- Content -->
+    <div class="container">
+
+        <!-- Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Historical Data
+                    <small>Visualize Quality</small>
+                </h1>
+                <p>Proactively envisioned multimedia based expertise and cross-media growth strategies. Seamlessly visualize quality intellectual capital without superior collaboration and idea-sharing. Holistically pontificate installed base portals after maintainable products.</p>
+            </div>
+        </div>
+        <!-- /.row -->
+		<div>
+		
+		<form action="ClientNames.php">
+			<input type="submit" value="List Clients" />
+		</form>
+		
+		<?php
+			$servername = "mydb.itap.purdue.edu";
+			$username = "g1109699";
+			$password = "MySQL27";
+			$dbname = "g1109699";
+
+			// Create connection
+			$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+			// Check connection
+			if ($conn-> connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+
+			$sql = "SELECT C_ID, company, cFirst FROM Client";
+
+			$result = mysqli_query($conn, $sql);
+
+			echo "<table border='1'>
+			<tr>
+			<th>Client ID</th>
+			<th>Company</th>
+			<th>First Name</th>
+			</tr>";
+
+			while($row = mysqli_fetch_array($result))
+			{
+			echo "<tr>";
+			echo "<td>" . $row['C_ID'] . "</td>";
+			echo "<td>" . $row['company'] . "</td>";
+			echo "<td>" . $row['cFirst'] . "</td>";
+			echo "</tr>";
+			}
+			echo "</table>" . "<br>";
+
+			$conn->close();
+
+		?>
+		
+		
+		
+		</div>
+        <!-- Feature Row -->
+        <div class="row">
+            <article class="col-md-4 article-intro">
+                <a href="#">
+                    <img class="img-responsive img-rounded" src="holder.js/700x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Efficiently nope</a>
+                </h3>
+                <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+            </article>
+            <article class="col-md-4 article-intro">
+                <a href="#">
+                    <img class="img-responsive img-rounded" src="holder.js/700x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Completely Synergize</a>
+                </h3>
+                <p>Dramatically maintain clicks-and-mortar solutions without functional solutions. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p>
+            </article>
+
+            <article class="col-md-4 article-intro">
+                <a href="#">
+                    <img class="img-responsive img-rounded" src="holder.js/700x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Dynamically Procrastinate</a>
+                </h3>
+                <p>Professionally cultivate one-to-one customer service with robust ideas. Completely synergize resource taxing relationships via premier niche markets. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
+            </article>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+	
+	<footer>
+		<div class="footer-blurb">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4 footer-blurb-item">
+						<h3><span class="glyphicon glyphicon-fire"></span> Dynamically Procrastinate</h3>
+						<p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+						<p><a class="btn btn-default" href="#">Procrastinate</a></p>
+					</div>
+					<div class="col-sm-4 footer-blurb-item">
+						<h3><span class="glyphicon glyphicon-cloud-upload"></span> Efficiently Unleash</h3>
+						<p>Dramatically maintain clicks-and-mortar solutions without functional solutions. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. </p>
+						<p><a class="btn btn-default" href="#">Unleash</a></p>
+					</div>
+					<div class="col-sm-4 footer-blurb-item">
+						<h3><span class="glyphicon glyphicon-leaf"></span> Completely Synergize</h3>
+						<p>Professionally cultivate one-to-one customer service with robust ideas. Completely synergize resource taxing relationships via premier niche markets. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
+						<p><a class="btn btn-default" href="#">Synergize</a></p>
+					</div>
+
+				</div>
+				<!-- /.row -->	
+			</div>
+        </div>
+        
+        <div class="small-print">
+        	<div class="container">
+        		<p><a href="#">Terms &amp; Conditions</a> | <a href="#">Privacy Policy</a> | <a href="#">Contact</a></p>
+        		<p>Copyright &copy; Group 27 2019 </p>
+        	</div>
+        </div>
+	</footer>
+
+	
+    <!-- jQuery -->
+    <script src="js/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+	
+	<!-- IE10 viewport bug workaround -->
+	<script src="js/ie10-viewport-bug-workaround.js"></script>
+	
+	<!-- Placeholder Images -->
+	<script src="js/holder.min.js"></script>
+	
+</body>
+
+</html>
