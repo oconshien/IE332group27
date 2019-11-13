f6028b20ea3e2a9eb8fb584ac5534ea12e1da4cd
jfinucane822 <jfinucane@att.net>
2019-11-13 16:52:56 -0500

    Making a Loop to run

--- g27/runMLClass.R
+++ g27/runMLClass.R
@@ -1,26 +1,38 @@
-
+#initialization
 budget <- 300000
 cityGrid <- buildCity(1)
 city_grid_radius <- 750
 MappedNetwork<- SA(budget, cityGrid, just_values = F)
-locationSen <- MappedNetwork$best
-value <- cityGrid[as.integer(locationSen[1,1]), as.integer(locationSen[1,2])]
+example <- MlClassifier()
 #Generated Pm Values
-
-date <- as.POSIXlt("2019-05-06 20:51:50 CET")
+locationSen <- MappedNetwork$best
+timefromSQL <- 1
+datefromSQL <- as.POSIXlt("2019-05-06 20:51:50 CET")
 points <- NULL
 point <- NULL
+dataframeForStates <- NULL
+bigData <- NULL
+datecnt <- 1
 
-i = 1
-while (i <= dim(locationSen)[1]){
-  point <- sortPM(Sys.time(), cityGrid[trunc(locationSen[i,1]/20) + city_grid_radius, trunc(locationSen[i,2]/20) + city_grid_radius])
-  points <- rbind(points, point)
-  i <- i + 1
+while(datecnt <= 24*timefromSQL){
+  points <- NULL
+  point <- NULL
+  dataframeForStates <- NULL
+  i = 1
+  while (i <= dim(locationSen)[1]){
+    #print(i)
+    point <- sortPM(datefromSQL, cityGrid[trunc(locationSen[i,1]/20) + city_grid_radius, trunc(locationSen[i,2]/20) + city_grid_radius])
+    points <- rbind(points, point)
+    i <- i + 1
+  }
+  #data_tester <- cbind(locationSen, points)
+  #Run MlClassifier Once then will be able to predict for any pm values
+  try <- data_label(points, example)
+  z <- priority_destinations(locationSen, try)
+  dataframeForStats <- data.frame(try, "x"=locationSen[,1], "y"=locationSen[,2])
+  bigData <- rbind(bigData, dataframeForStats)
+  updates <- nearest_sensor(z, locationSen, 0)
+  locationSen <- updates
+  datefromSQL <- datefromSQL + 60*60
+  datecnt <- datecnt + 1
 }
-test <- data.frame("pm010"=100, "pm025"=100, "pm100"=100)
-#data_tester <- cbind(locationSen, points)
-#Run MlClassifier Once then will be able to predict for any pm values
-example <- MlClassifier()
-try <- data_label(points, example)
-z <- priority_destinations(locationSen, try)
-updates <- nearest_sensor(z, locationSen, 0)
