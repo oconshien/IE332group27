dc6f79601e5c1ce65adf24b29d651a95363081c5
TrumanBoggs <55259630+Truman1928@users.noreply.github.com>
2019-11-21 12:42:07 -0500

    Update Indianapolis.R

--- g27/sample cities/Indianapolis.R
+++ g27/sample cities/Indianapolis.R
@@ -1,5 +1,5 @@
 geoRadius <- 15000/20
-cityGrid <- matrix(rep(-1,(2*geoRadius)^2), 2*geoRadius, 2*geoRadius)
+cityGrid <- matrix(nrow = geoRadius * 2, ncol = geoRadius * 2)
 for(i in -geoRadius:geoRadius){
   for(j in -geoRadius:geoRadius){
     cityGrid[i+geoRadius,j+geoRadius] <- 1
@@ -8,15 +8,36 @@ for(i in -geoRadius:geoRadius){
 
 #Indianapolis City Center
 for(x in 750:900){
-  for(y in 700:950){
+  for(y in 600:820){
       cityGrid[x,y] <- 2
   }
 }
 
+#Rural Bottom Left
+for(x in 0:540){
+  for(y in 0:480){
+    cityGrid[x,y] <- 0
+  }
+}
+
+#Rural Top Left
+for(x in 0:300){
+  for(y in 1250:1500){
+    cityGrid[x,y] <- 0
+  }
+}
+
+#Rural Top Right
+for(x in 1300:1500){
+  for(y in 1250:1500){
+    cityGrid[x,y] <- 0
+  }
+}
+
 #Rural Bottom Right
-for(x in 0:900){
-  for(y in 850:1500){
-    cityGrid[x,y] <- 2
+for(x in 1340:1500){
+  for(y in 0:260){
+    cityGrid[x,y] <- 0
   }
 }
 
